7e9c303f8aad309dff7c5b9d437c869b2f19f893
Tery Tsai <iliveinabucket@berkeley.edu>
2018-05-14 15:47:05 -0700

    Have Cecil's changes but Use default props so onClose prop doesn't have to be specified

--- src/index.ios.js
+++ src/index.ios.js
@@ -9,44 +9,63 @@ import {
   TouchableWithoutFeedback,
   View
 } from "react-native";
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from "prop-types";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end"
   },
   modalContent: {
     padding: 0,
-    backgroundColor: '#d1d5db',
+    backgroundColor: "#d1d5db"
   },
   textContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   text: {
-    textAlign: 'center',
-    flex: 1,
+    textAlign: "center",
+    flex: 1
   },
   buttonBar: {
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
     borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: 'lightgrey',
-    backgroundColor: '#f0f0f2',
+    borderTopColor: "lightgrey",
+    backgroundColor: "#f0f0f2"
   },
   buttonView: {
-    padding: 12,
+    padding: 12
   },
   buttonText: {
-    color: '#006BFF',
+    color: "#006BFF",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold"
   }
 });
 
 class PickerModal extends React.Component {
+  static propTypes = {
+    doneLabel: PropTypes.string,
+    onClose: PropTypes.func,
+    onValueChange: PropTypes.func,
+    style: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.number,
+      PropTypes.array
+    ])
+  };
+
+  static defaultProps = {
+    onClose: () => {},
+    onValueChange: () => {},
+    style: null,
+    doneLabel: "Done"
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -68,6 +87,7 @@ class PickerModal extends React.Component {
   }
 
   closePicker() {
+    this.props.onClose();
     this.setState({ open: false });
   }
 
@@ -77,8 +97,10 @@ class PickerModal extends React.Component {
 
   getSelectedItem() {
     const childrenArray = React.Children.toArray(this.props.children);
-    let selected = childrenArray.find(child => child.props.value === this.props.selectedValue);
-    
+    let selected = childrenArray.find(
+      child => child.props.value === this.props.selectedValue
+    );
+
     if (selected) {
       return selected;
     }
@@ -91,20 +113,26 @@ class PickerModal extends React.Component {
     delete props.style;
     delete props.children;
     delete props.onValueChange;
-    
+
     const picker = (
       <Modal
-        animationType={'slide'}
+        animationType={"slide"}
         visible={this.state.open}
         transparent={true}
-        onRequestClose={this.bindings.togglePicker}>
-        <TouchableWithoutFeedback onPress={this.bindings.closePicker}>  
+        onRequestClose={this.bindings.togglePicker}
+      >
+        <TouchableWithoutFeedback onPress={this.bindings.closePicker}>
           <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
               <View style={styles.buttonBar}>
-                <ModalButton label="Done" onPress={this.bindings.closePicker} />
-              </View>  
-              <Picker {...props} onValueChange={this.bindings.onValueChange}>{this.props.children}</Picker>
+                <ModalButton
+                  label={props.doneLabel}
+                  onPress={this.bindings.closePicker}
+                />
+              </View>
+              <Picker {...props} onValueChange={this.bindings.onValueChange}>
+                {this.props.children}
+              </Picker>
             </View>
           </View>
         </TouchableWithoutFeedback>
@@ -113,9 +141,15 @@ class PickerModal extends React.Component {
 
     return (
       <View>
-        <TouchableHighlight underlayColor={'#DDD'} style={[styles.textContainer, this.props.style]} onPress={this.bindings.togglePicker}>
+        <TouchableHighlight
+          underlayColor={"#DDD"}
+          style={[styles.textContainer, this.props.style]}
+          onPress={this.bindings.togglePicker}
+        >
           <View style={[styles.textContainer, this.props.style]}>
-            <Text style={styles.text}>{this.getSelectedItem().props.label}</Text>
+            <Text style={styles.text}>
+              {this.getSelectedItem().props.label}
+            </Text>
             <Icon name="arrow-drop-down" size={30} color="black" />
           </View>
         </TouchableHighlight>
@@ -130,13 +164,11 @@ class ModalButton extends React.Component {
     const { label, onPress } = this.props;
     return (
       <TouchableOpacity onPress={onPress}>
-        <View style={styles.buttonView }>
-          <Text
-            allowFontScaling={false}
-            style={styles.buttonText}>
+        <View style={styles.buttonView}>
+          <Text allowFontScaling={false} style={styles.buttonText}>
             {label}
           </Text>
-        </View>  
+        </View>
       </TouchableOpacity>
     );
   }
